be79abd707fb41aed5c6caa59e80871395e9f8c0
DesertChaos <vadimsysolin52565@gmail.com>
2025-02-13 21:03:30 +0300

    Create test3.cs
    heheheheheh

---
+++ test3.cs
@@ -0,0 +1,36 @@
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Попробуйте угадать число от 1 до 100");
+            
+            Random rnd = new Random();
+            int goal = rnd.Next(1, 101);
+            Console.WriteLine($"По секрету, это число - {goal}");
+            int counter = 0; //счетчик попыток пользователя
+
+            start:  //оказывается можно ставить метки просто так. приколь конечно
+                Console.WriteLine("Введите число");
+                int answer = Convert.ToInt32(Console.ReadLine());
+                counter++;
+
+            switch (answer)
+            {
+                case int n when n == goal && counter < 5:
+                    Console.WriteLine("Верно");
+                    break;
+
+                case int x when x != goal && counter > 4:
+                    Console.WriteLine("Попытки закончились");
+                    break;
+
+                default:
+                    Console.WriteLine("Неверно");                   
+                    goto start;
+            }
+
+        }
+    }
+}
